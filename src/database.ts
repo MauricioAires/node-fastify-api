59138afce7cc7eb8aef0d8f5dc6e850cac091f65
MauricioAires <mauricioaires015@gmail.com>
2023-11-10 19:04:34 -0300

    feat(database) config knex sqlite

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: env.DATABASE_URL,
   },
   // Por padrão todos os campos do banco vair ser null
   useNullAsDefault: true,
